a8a17e40d0730d933867001fb0073226b83ef3c1
Michael Entrup (b. Epping) <mail@m-entrup.de>
2019-10-12 17:13:39 +0200

    Enhancing testing_1.py:
    Running replace_by_image() a second time.
    verifying the second run does not create new shapes.

--- playground/testing_1.py
+++ playground/testing_1.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import List
 
 from pptx import Presentation
 from pptx.shapes.autoshape import BaseShape
@@ -26,15 +27,29 @@ slide_info = [get_shape_info(shape) for shape in prs.slides[0].shapes]
 print(slide_info)
 
 
-def replace_by_image(slide: Slide, name: str, img_path: Path) -> None:
-    shapes_to_replace = [shape for shape in slide.shapes if shape.text == name]
+def replace_by_image(slide: Slide, name: str, img_path: Path) -> List[BaseShape]:
+    shapes_to_replace = [shape for shape in slide.shapes if hasattr(shape, "text") and shape.text == name]
+    print(len(shapes_to_replace))
+    new_image_shapes = []
     for old_shape in shapes_to_replace:
         shape_info = get_shape_info(old_shape)
         print(shape_info)
         img_file = open(img_path, "rb")
-        slide.shapes.add_picture(img_file, old_shape.left, old_shape.top, old_shape.width, old_shape.height)
-        old_shape._parent.element.remove(old_shape.element)
+        slide_shapes = old_shape._parent
+        img_shape = slide_shapes.add_picture(
+            img_file,
+            old_shape.left,
+            old_shape.top,
+            old_shape.width,
+            old_shape.height
+        )
+        new_image_shapes.append((img_shape))
+        slide_shapes.element.remove(old_shape.element)
+    return new_image_shapes
 
 
-replace_by_image(prs.slides[0], "#logo", Path("./playground/pptx_icon.png"))
+added_img_shapes = replace_by_image(prs.slides[0], "#logo", Path("./playground/pptx_icon.png"))
+assert len(added_img_shapes) == 1
+added_img_shapes = replace_by_image(prs.slides[0], "#logo", Path("./playground/pptx_icon.png"))
+assert len(added_img_shapes) == 0
 prs.save(Path("./playground") / example_file_path.name)
